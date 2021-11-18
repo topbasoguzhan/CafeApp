5496ef68f45890bdd455a7cae0071e15e3ee0632
Oğuzhan Topbaş <topbasoguzhan@gmail.com>
2021-11-18 10:08:19 +0300

    tasarım changes

--- CafeApp/BeginForm.Designer.cs
+++ CafeApp/BeginForm.Designer.cs
@@ -39,11 +39,14 @@
             // btnKaydet1
             // 
             this.btnKaydet1.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.btnKaydet1.Location = new System.Drawing.Point(95, 99);
+            this.btnKaydet1.BackgroundImage = global::CafeApp.Properties.Resources.istockphoto_816807384_612x612;
+            this.btnKaydet1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnKaydet1.Location = new System.Drawing.Point(125, 94);
             this.btnKaydet1.Name = "btnKaydet1";
-            this.btnKaydet1.Size = new System.Drawing.Size(542, 312);
+            this.btnKaydet1.Size = new System.Drawing.Size(529, 345);
             this.btnKaydet1.TabIndex = 0;
             this.btnKaydet1.Text = "Kaydet";
+            this.btnKaydet1.TextAlign = System.Drawing.ContentAlignment.BottomRight;
             this.btnKaydet1.UseVisualStyleBackColor = true;
             this.btnKaydet1.Click += new System.EventHandler(this.btnKaydet1_Click);
             // 
@@ -51,7 +54,7 @@
             // 
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(144, 9);
+            this.label1.Location = new System.Drawing.Point(167, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(412, 20);
             this.label1.TabIndex = 1;
@@ -59,35 +62,50 @@
             // 
             // label2
             // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AllowDrop = true;
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(77, 52);
+            this.label2.Location = new System.Drawing.Point(131, 55);
             this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.label2.Size = new System.Drawing.Size(130, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "Kat sayısını giriniz:";
             // 
             // txtKatBilgisi
             // 
-            this.txtKatBilgisi.Location = new System.Drawing.Point(213, 49);
+            this.txtKatBilgisi.AllowDrop = true;
+            this.txtKatBilgisi.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtKatBilgisi.Location = new System.Drawing.Point(267, 52);
             this.txtKatBilgisi.Name = "txtKatBilgisi";
+            this.txtKatBilgisi.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.txtKatBilgisi.Size = new System.Drawing.Size(125, 27);
             this.txtKatBilgisi.TabIndex = 3;
             // 
             // label3
             // 
+            this.label3.AllowDrop = true;
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(395, 49);
+            this.label3.Location = new System.Drawing.Point(398, 55);
             this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.label3.Size = new System.Drawing.Size(127, 20);
             this.label3.TabIndex = 4;
             this.label3.Text = "Kafe Adını Giriniz:";
             // 
             // txtKafeAdi
             // 
-            this.txtKafeAdi.Location = new System.Drawing.Point(528, 49);
+            this.txtKafeAdi.AllowDrop = true;
+            this.txtKafeAdi.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtKafeAdi.Location = new System.Drawing.Point(531, 52);
             this.txtKafeAdi.Name = "txtKafeAdi";
-            this.txtKafeAdi.Size = new System.Drawing.Size(100, 27);
+            this.txtKafeAdi.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtKafeAdi.Size = new System.Drawing.Size(123, 27);
             this.txtKafeAdi.TabIndex = 5;
             this.txtKafeAdi.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -95,6 +113,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 451);
             this.Controls.Add(this.txtKafeAdi);
             this.Controls.Add(this.label3);
